8619e6d2b6bae3ca9b042d951e3531a019452e80
Sohail Khan <Sohailkhan2k22@gmail.com>
2025-02-16 07:03:32 +0500

    sqlite db connection

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from  flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy.sql import func  # Import func
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress a warning
+
+db =  SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno          = db.Column(db.Integer, primary_key=True)
+    title        = db.Column(db.String(200), nullable=False)
+    description  = db.Column(db.String(500), nullable=False)  # Fixed data type
+    date_created = db.Column(db.DateTime, default=func.now())  # Fixed datetime reference
+
+
 
 @app.route("/")
 def hello_world():
