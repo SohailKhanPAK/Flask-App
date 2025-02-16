07134203d1901f8bfb0db5754d64c723108bcbb2
Sohail Khan <Sohailkhan2k22@gmail.com>
2025-02-16 23:15:00 +0500

    feat :  Delete Operation done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template , request , redirect
 from  flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy.sql import func  # Import func
@@ -25,17 +25,25 @@ def hello_world():
     if request.method == 'POST':
         title =  request.form['title']
         description = request.form['description']
-    todo = Todo(Title=title , Description=description)
-    db.session.add(todo)
-    db.session.commit()
+        todo = Todo(Title=title , Description=description)
+        db.session.add(todo)
+        db.session.commit()
     all_todos = Todo.query.all()
     return render_template("index.html" , all_todos=all_todos)
 
-@app.route("/show")
-def products():
-    all_todo = Todo.query.all()
-    return "<p>This is Porduct Page! </p>"
+@app.route("/update")
+def update_data():
+    # todo_item = Todo.query.filter_by(SNO = SNO).first()
+    # db.session.delete(todo_item)
+    # db.session.commit()
+    return redirect("/")
 
+@app.route("/delete/<int:SNO>")
+def delete_data(SNO):
+    todo_item = Todo.query.filter_by(SNO=SNO).first()
+    db.session.delete(todo_item)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
